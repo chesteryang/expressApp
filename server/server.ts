d885e0267d6d7691aa41c7b4b391a43c8023b41d
Chester Yang <chester_cyyang@hotmail.com>
2018-08-06 10:32:50 -0600

    Add GraphQL server and pass unit tests

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import { Server } from 'http';
 import express, { Express } from 'express';
 import * as dotenv from 'dotenv';
 import * as bodyParser from 'body-parser';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema, GraphQLSchema } from 'graphql';
 
 import * as homeController from './controllers/home.controller';
 
@@ -12,8 +14,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', homeController.index);
 
+const schema: GraphQLSchema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  }
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}));
+
 const server: Server = app.listen(app.get('port'), () => {
   console.log(('App is running at http://localhost:%d in %s mode'),
+    app.get('port'), app.get('env'));
+  console.log(('App GraphQL server is running at http://localhost:%d/graphql in %s mode'),
     app.get('port'), app.get('env'));
   console.log('Press CTRL-C to stop\n');
 });
--- tests/server/home.test.ts
+++ tests/server/home.test.ts
@@ -11,8 +11,32 @@ describe('GET /', () => {
         expect(res.body).have.property('message');
       });
   });
+});
 
-  afterAll(() => {
-      server.close();
+describe('GET /graphql?query={hello}', () => {
+  it('should return 200 OK', () => {
+    return supertest(server)
+      .get('/graphql?query={hello}')
+      .expect(200)
+      .then((res) => {
+        expect(res.body).have.property('data');
+      });
   });
+});
+
+describe('POST /graphql with query data', () => {
+  it('should return 200 OK', () => {
+    return supertest(server)
+      .post('/graphql')
+      .set('Content-Type', 'application/json')
+      .send('{"query": "{ hello }"}')
+      .expect(200)
+      .then((res) => {
+        expect(res.body).have.property('data');
+      });
+  });
+});
+
+afterAll(() => {
+  server.close();
 });
